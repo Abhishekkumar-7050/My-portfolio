3f166eebffebd69c29517f1afcc8cc44ded2b63a
Abhishekkumar-7050 <abhishekofficial7050>
2024-05-20 02:06:35 +0530

    basic react app ready with Tailwind css

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import logoImg  from './images/logo.png'
+import pageWhite  from './images/page_white_magnify.png'
+import quote  from './images/quote.png'
+import bannerImg  from './images/bannerImg.png'
+import project1  from './images/project1.png'
+import project2  from './images/project2.png'
+import project3  from './images/project3.png'
+
+
+
+export  {
+    logoImg,
+    pageWhite,
+    quote,
+    bannerImg,
+    project1,
+    project2,
+    project3
+}
+
