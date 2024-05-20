7b6759126c0250ac0236e76bc3c7b4f1db594fd5
Abhishekkumar-7050 <abhishekofficial7050>
2024-05-20 16:35:26 +0530

    adding the banner and hero page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
 import React from 'react'
 import './App.css'
-
+import Navbar from './components/navbar/Navbar'
+import Banner from './components/banner/Banner'
+import Features from './components/features/Features'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='w-full h-auto bg-bodyColor text-lightText'>
+      <div className='max-w-screen-2xl mx-auto px-16 '>
+       <Navbar/>
+       <Banner/>
+       <Features/>
+      </div>
+
+    </div>
+  
     
   
-    <h1 className="text-3xl font-bold underline py-3">
-      Hello world!
-    </h1>
-  
 
-    </>
+    
   )
 }
 
